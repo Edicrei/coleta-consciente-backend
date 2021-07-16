bbb4d78af9d60b239e27f0a699ec01c6b1156856
aagm404 <aagmerlini@gmail.com>
2021-07-16 02:16:21 -0300

    Swagger documentation

--- location/src/main/java/com/coletaconsciente/location/api/controller/LocationController.java
+++ location/src/main/java/com/coletaconsciente/location/api/controller/LocationController.java
@@ -6,59 +6,26 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.coletaconsciente.location.api.controller.LocationController;
 import com.coletaconsciente.location.domain.model.Location;
-import com.coletaconsciente.location.domain.service.LocationService;
 
-@RestController
-@RequestMapping("/location")
-public class LocationController {
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
-	@Autowired
-	private LocationService locationService;
+@ApiOperation(value = "Location Controller", notes = "API for Location service")
+@ApiResponses(value = {
+        @ApiResponse(code = 200, message = "OK"),
+        @ApiResponse(code = 400, message = "error, verify your request and try again")
+})
+public interface LocationController {
 
-	@GetMapping
-	public List<Location> findAll() {
-		return locationService.findAll();
-	}
-
-	@GetMapping("/{locationId}")
-	public Location findById(@PathVariable("locationId") Long id) {
-		return locationService.findById(id);
-	}
-
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public Location save(@RequestBody @Valid Location location) {
-		return locationService.save(location);
-	}
-
-	@PutMapping("/{locationId}")
-	public Location update(@PathVariable Long locationId, @RequestBody @Valid Location location) {
-		return locationService.update(locationId, location);
-	}
-	
-	@PatchMapping("/{locationId}")
-	public Location partilUpdate(@PathVariable Long locationId, @RequestBody Map<String, Object> fields, HttpServletRequest request) {
-		return locationService.partilUpdate(locationId, fields, request);
-	}
-
-	@DeleteMapping("/{locationId}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void remove(@PathVariable Long locationId) {
-		locationService.remove(locationId);
-	}
+	public List<Location> findAll();
+	public Location findById(@PathVariable("locationId") Long id);
+	public Location save(@RequestBody @Valid Location location);
+	public Location update(@PathVariable Long locationId, @RequestBody @Valid Location location);
+	public Location partilUpdate(@PathVariable Long locationId, @RequestBody Map<String, Object> fields, HttpServletRequest request);
+	public void remove(@PathVariable Long locationId);
 }
